ec8682883646de1548c7e2b4623cf83c6d0f5291
Paul Warren <paul@paulwarren.ca>
2018-11-05 23:11:47 -0500

    feat: initial code
    Adds four functions:
    asMoney
    showMoney
    gen_uuid
    dot_explode

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-common-helpers
+# Common Helpers
+
+A collection of things that go in the helpers file for various projects.
+
+* gen_uuid = Generates a unique ID in the format of: c2ce3a4d-e729-4d76-8af3-95a982fa5f62
+
+* showMoney = Does the same as asMoney, but has more configuration options and also shows the currency symbol.
+
+* asMoney = Converts a value to a double with two decimal points and no thousands separator.
+
+* dot_explode($array) = convert a dot based flat array to a multidimensional array.
---
+++ src/helpers.php
@@ -0,0 +1,80 @@
+<?php
+
+if ( ! function_exists('asMoney')) {
+    function asMoney($value)
+    {
+        $value = (double)$value;
+
+        return (double)number_format($value, 2, '.', '');
+    }
+}
+
+if ( ! function_exists('showMoney')) {
+    function showMoney($value, $format = null, $showSymbols = true)
+    {
+        $value = (double)$value;
+        if ($format === null) {
+            $format = config('settings.global.money_format', '%i');
+        }
+
+        // return \money_format( $format, $this->value)
+        if ( ! function_exists('money_format')) {
+            return ($showSymbols ? config('settings.global.pre_currency_symbol') : '') .
+                   number_format($value, 2, '.', $showSymbols ? ',' : '') .
+                   ($showSymbols ? config('settings.global.post_currency_symbol') : '');
+        } else {
+            // As of this moment, this is not available on windows. The languages are also not available when I used a custom function from http://www.php.net/manual/en/function.money-format.php
+            setlocale(LC_MONETARY, config('settings.global.money_locale', 'en_CA.utf8'));
+
+            return money_format($format, $value);
+        }
+    }
+}
+
+if ( ! function_exists('gen_uuid')) {
+    function gen_uuid()
+    {
+        return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            // 32 bits for "time_low"
+            mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+
+            // 16 bits for "time_mid"
+            mt_rand(0, 0xffff),
+
+            // 16 bits for "time_hi_and_version",
+            // four most significant bits holds version number 4
+            mt_rand(0, 0x0fff) | 0x4000,
+
+            // 16 bits, 8 bits for "clk_seq_hi_res",
+            // 8 bits for "clk_seq_low",
+            // two most significant bits holds zero and one for variant DCE1.1
+            mt_rand(0, 0x3fff) | 0x8000,
+
+            // 48 bits for "node"
+            mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+        );
+    }
+}
+
+if ( ! function_exists('dot_explode')) {
+    function dot_explode($array)
+    {
+        $newArray = [];
+        foreach ($array as $key => $value) {
+            $exploded = explode('.', $key);
+
+            $subArray = [];
+            $refArray = &$subArray;
+            foreach ($exploded as $subkey) {
+                $refArray = &$refArray[$subkey];
+            }
+            $refArray = $value;
+
+            $newArray = array_merge_recursive($subArray, $newArray);
+
+            unset($subArray);
+        }
+
+        return $newArray;
+    }
+}
